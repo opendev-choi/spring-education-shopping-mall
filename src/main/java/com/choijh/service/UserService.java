ca7e396ae78e4f11046cfb90524003974237dde4
JongHyeok Choi <opendev.choi@gmail.com>
2020-12-14 15:17:15 +0900

    feat: add service initialize and findAll

---
+++ src/main/java/com/choijh/service/UserService.java
@@ -0,0 +1,47 @@
+package com.choijh.service;
+
+import com.choijh.model.User;
+import com.choijh.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void findAll() {
+        for (User user : this.userRepository.findAll()) {
+            System.out.println(user);
+        }
+    }
+
+    public void initializeUsers() {
+        User user1 = User.builder()
+                .email("example1@sample.com")
+                .name("Mr. Example")
+                .phone("01000000000")
+                .build();
+
+        User user2 = User.builder()
+                .email("example2@sample.com")
+                .name("Mrs. Sample")
+                .phone("01000001234")
+                .build();
+
+        User user3 = User.builder()
+                .email("example3@sample.com")
+                .name("ms. Sample Data")
+                .phone("01012341234")
+                .build();
+
+        this.userRepository.save(user1);
+        this.userRepository.save(user2);
+        this.userRepository.save(user3);
+        this.userRepository.flush();
+    }
+}
