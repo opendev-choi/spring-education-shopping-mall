835a021c293c3c9e034d9546cb734ac4faba88de
JongHyeok Choi <opendev.choi@gmail.com>
2020-12-15 13:52:56 +0900

    feat: add product route

---
+++ src/main/java/com/choijh/route/ProductRoute.java
@@ -0,0 +1,29 @@
+package com.choijh.route;
+
+import com.choijh.model.Product;
+import com.choijh.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductRoute {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductRoute(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{product_id}")
+    @ResponseBody
+    public Product getProduct(@PathVariable(value="product_id") String productId) throws Exception {
+        return this.productService.find(Integer.parseInt(productId));
+    }
+
+    @GetMapping("/initialize")
+    public void initializeProducts() {
+        this.productService.initializeProducts();
+    }
+
+}
