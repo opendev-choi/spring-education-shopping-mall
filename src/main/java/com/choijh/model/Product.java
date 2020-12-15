24361f21a33afbe5351bae3f8e5f9829f6a3a1cb
JongHyeok Choi <opendev.choi@gmail.com>
2020-12-15 09:20:09 +0900

    feat: add product jpa object

---
+++ src/main/java/com/choijh/model/Product.java
@@ -0,0 +1,44 @@
+package com.choijh.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    @Column(length = 80, nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private int listPrice;
+
+    @Column
+    private int price;
+
+    @Builder
+    public Product(String name, String description, int listPrice, int price) {
+        this.name = name;
+        this.description = description;
+        this.listPrice = listPrice;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Product[productId='%s', name='%s', description='%s', listPrice=%d, price=%d]",
+                this.productId, this.name, this.description, this.listPrice, this.price
+        );
+    }
+}
