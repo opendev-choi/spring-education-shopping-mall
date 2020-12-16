07f04b31bc352edcbfe62d7315506991dc21980f
JongHyeok Choi <opendev.choi@gmail.com>
2020-12-16 10:12:16 +0900

    feat: add refund to sale service

--- src/main/java/com/choijh/service/SaleService.java
+++ src/main/java/com/choijh/service/SaleService.java
@@ -46,8 +46,13 @@ public class SaleService {
         this.saleRepository.flush();
     }
 
-    public void refund(int orderId) {
+    public void refund(int saleId) throws Exception{
+        Optional<Sale> targetSale = this.saleRepository.findById(saleId);
+        Sale sale = targetSale.orElseThrow(() -> new Exception("결제 취소로 변경하는 도중에 문제가 생겼습니다"));
 
+        sale.setStatus(SaleStatusEnum.REFUNDED);
+        this.saleRepository.save(sale);
+        this.saleRepository.flush();
     }
 
     public void initializeSales() {
