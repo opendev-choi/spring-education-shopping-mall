1b1b6cd46670383971bde9a6bf2f8caa5cf90254
JongHyeok Choi <opendev.choi@gmail.com>
2020-12-18 11:37:27 +0900

    feat: add create coupon service

---
+++ src/main/java/com/choijh/service/CouponService.java
@@ -0,0 +1,45 @@
+package com.choijh.service;
+
+import com.choijh.model.Coupon;
+import com.choijh.repository.CouponRepository;
+import com.choijh.vo.CouponRegisterVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Controller
+public class CouponService {
+    private final CouponRepository couponRepository;
+
+    @Autowired
+    public CouponService(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
+    }
+
+    public int createCoupon(CouponRegisterVO couponRegisterVO) throws Exception {
+        if (couponRegisterVO.getDiscountPercentage() != 0 && couponRegisterVO.getDiscountPrice() != 0) {
+            throw new Exception("할인 금액과 할인 비율이 동시에 존재할수 없습니다!");
+        }
+
+        Coupon createdCoupon = Coupon.builder()
+                .availableDays(couponRegisterVO.getAvailableDays())
+                .category(couponRegisterVO.getCategory())
+                .discountPercentage(couponRegisterVO.getDiscountPercentage())
+                .discountPrice(couponRegisterVO.getDiscountPrice())
+                .expireAt(couponRegisterVO.getExpireAt())
+                .productID(couponRegisterVO.getProductID())
+                .build();
+
+        this.couponRepository.save(createdCoupon);
+        this.couponRepository.flush();
+
+        return createdCoupon.getCouponId();
+    }
+
+    public Coupon couponById(int couponId) throws Exception {
+        Optional<Coupon> coupon = this.couponRepository.findById(couponId);
+        
+        return coupon.orElseThrow(() -> new Exception("해당 쿠폰을 확인할수 없습니다"));
+    }
+}
---
+++ src/main/java/com/choijh/vo/CouponRegisterVO.java
@@ -0,0 +1,29 @@
+package com.choijh.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CouponRegisterVO {
+    Date expireAt;
+    int availableDays;
+    String productID;
+    String category;
+    int discountPrice = 0;
+    int discountPercentage = 0;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "CouponRegisterVO[expireAt='%s', availableDays=%d, productID=%d, category='%s', " +
+                        "discountPrice=%d, discountPercentage=%d]",
+                this.expireAt, this.availableDays, this.productID, this.category, this.discountPrice,
+                this.discountPercentage
+        );
+    }
+}
