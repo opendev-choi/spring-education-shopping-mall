af03404db991ecd69a0989bd4047bb3de78e06f8
JongHyeok Choi <opendev.choi@gmail.com>
2020-12-18 13:15:55 +0900

    feat: create issueCoupon Repository and Model

---
+++ src/main/java/com/choijh/vo/IssueCouponVO.java
@@ -0,0 +1,21 @@
+package com.choijh.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class IssueCouponVO {
+    int couponId;
+    int userId;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "IssueCouponVO[couponId=%d, userId=%d]",
+                this.couponId, this.userId
+        );
+    }
+}
