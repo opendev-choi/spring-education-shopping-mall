958c82648c167ebac270f5ec22726617752377ca
JongHyeok Choi <opendev.choi@gmail.com>
2020-12-18 10:43:32 +0900

    feat: add CouponRepository

---
+++ src/main/java/com/choijh/repository/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.choijh.repository;
+
+import com.choijh.model.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Integer> {
+}
